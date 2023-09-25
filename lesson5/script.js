bddf692d6137a96013a785e43f213be6c019edf6
Bogdan Bikbulatov <batmansila1234@gmail.com>
2023-09-25 10:22:31 +0300

    javascript

---
+++ lesson5/script.js
@@ -0,0 +1,109 @@
+// console.log("") vivodit v konsol`
+// alert("") vivodit okno
+// [1,2,3].forEach(alert) ;
+
+//                 Variables
+
+// var
+// let
+// const
+
+// var hello = 'hello world';
+// console.log(hello);
+//
+// var times = 5
+// if (times > 1){
+//     var hello = 'New hello world';
+// }
+// console.log(hello);
+
+// let hello = 'hello world';
+// console.log(hello);
+//
+// var times = 5;
+// if(times > 1){
+//     hello = 'new hello world';
+// }
+// console.log(hello);
+
+// console.log(hello);
+// var hello = 'hello';
+// let hello = 'hello';
+
+// let total = 0;
+// console.log(total);
+// total += 600;
+// total = 600;
+// console.log(total);
+
+// const DISCOUNT = 10;
+// console.log(DISCOUNT);
+// let newDiscount = DISCOUNT + 5;
+// console.log(newDiscount);
+// discount = 5; ninada const menyat`
+
+// let user1;
+// let 1user;
+// let user1 = 'germany';
+
+//                Data Type
+// let a = '5';
+// let b = '3';
+// console.log(a + b)
+
+//    NUMBER
+// let total = 0;
+// total = 12;
+// total = 12.5534;
+// console.log( total / 0 ); // infinity
+// console.log('text' / 2); // NaN
+// console.log( '5' / 2 ); // 2.5
+// console.log( '5' + 2); // 52
+// console.log(2 + '5'); // 25
+
+//   STRING
+// let hello = 'hello';
+// let world = "Tom`s group";
+// let phrase = `This is ${hello}!`;
+// console.log(phrase);
+
+// Boolean
+// let isGreateThanFive = true
+// let isSubscribed = false;
+//
+// console.log(isGreateThanFive);
+// isGreateThanFive = 4 > 5;
+// console.log(isGreateThanFive);
+
+//   Null
+// let age = null;
+
+
+//  Undefined
+// let age;
+// console.log(age);
+
+
+              // typeof( ... );
+// Objects
+
+
+
+
+
+// ...
+// let age = prompt('How old are you?', 100)
+// console.log(age);
+
+// let isAdmin = confirm('Do you have admin permissions?');
+// console.log(isAdmin);
+
+// let isAdmin = confirm('admin?')
+// if(isAdmin){
+//     document.getElementById('test').innerHTML('admin')
+// } else document.getElementById('test').innerHTML('ne admin')
+
+// let age = prompt('you age?')
+// if(age > 100){
+//     console.log('Are you sure?')
+// } else console.log('welcome to the page!')
