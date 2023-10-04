ef74aa111789fb08d2be27097d85079260fccf06
Bogdan Bikbulatov <batmansila1234@gmail.com>
2023-10-04 10:20:47 +0300

    todo js

--- homework-todo/script.js
+++ homework-todo/script.js
@@ -16,41 +16,50 @@ document.addEventListener('DOMContentLoaded', function (){
     function ProcessHandler() {
         let nextStepProcessElements = document.querySelectorAll('.nextStepProcess');
         nextStepProcessElements.forEach(function(element) {
-            element.addEventListener('click', function() {
-                let el = element.parentNode;
-                const text = el.innerText.split(' В процессі')[0];
-                el.remove();
-                const process = document.getElementById('process');
-                const newTask = document.createElement("p");
-                newTask.innerHTML = text + ' <span class="nextStepDone">Зроблено</span>';
-                process.appendChild(newTask);
-                DoneHandler();
-            });
+            element.removeEventListener('click', processClick);
+            element.addEventListener('click', processClick);
         });
+    }
+
+    function processClick() {
+        let el = this.parentNode;  // Use "this" to refer to the clicked element
+        const text = el.innerText.split(' В процессі')[0];
+        el.remove();
+        const process = document.getElementById('process');
+        const newTask = document.createElement("p");
+        newTask.innerHTML = text + ' <span class="nextStepDone">Зроблено</span>';
+        process.appendChild(newTask);
+        DoneHandler();
     }
 
     function DoneHandler() {
         let nextStepDoneElements = document.querySelectorAll('.nextStepDone');
         nextStepDoneElements.forEach(function(element) {
-            element.addEventListener('click', function() {
-                let el = element.parentNode;
-                const text = el.innerText.split(' Зроблено')[0];
-                el.remove();
-                const done = document.getElementById('done');
-                const newTask = document.createElement("p");
-                newTask.innerHTML = text + ' <span class="delete">Видалити</span>';
-                done.appendChild(newTask);
-                DeleteHandler();
-            });
+            element.removeEventListener('click', doneClick);
+            element.addEventListener('click', doneClick);
         });
+    }
+
+    function doneClick() {
+        let el = this.parentNode;
+        const text = el.innerText.split(' Зроблено')[0];
+        el.remove();
+        const done = document.getElementById('done');
+        const newTask = document.createElement("p");
+        newTask.innerHTML = text + ' <span class="delete">Видалити</span>';
+        done.appendChild(newTask);
+        DeleteHandler();
     }
 
     function DeleteHandler() {
         let deleteElements = document.querySelectorAll('.delete');
         deleteElements.forEach(function(element) {
-            element.addEventListener('click', function() {
-                element.parentNode.remove();
-            });
+            element.removeEventListener('click', deleteClick);
+            element.addEventListener('click', deleteClick);
         });
+    }
+
+    function deleteClick() {
+        this.parentNode.remove();
     }
 })
